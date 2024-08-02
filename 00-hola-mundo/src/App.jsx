ab9acaae529d8b9e3c10de7e56a144a5fcb476e7
Juan Garcia <juangarcia@juans-air.lan>
2024-08-02 15:10:23 +0100

    first commit

---
+++ 00-hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+export function App () {
+  return (
+   <article>
+    <header>
+      <img src="https://unavatar.io/midudev" alt="El avatar de midudev" />
+      <div>
+        <strong>Miguel Ángel Durán</strong>
+        <span>@midudev</span>
+      </div>
+    </header>
+
+    <aside>
+      <button>
+        Seguir
+      </button>
+    </aside>
+   </article>
+  )
+}
